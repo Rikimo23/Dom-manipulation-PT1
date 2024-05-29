51085bc1b1b0011583f36029ba65cf86d7d2e9bf
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-05-29 12:34:48 -0400

    sorry not done

--- java.js
+++ java.js
@@ -14,3 +14,32 @@ mainEl.style.background = "var(--main-bg)";
 
 //Inject an h1 with text into main
 mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+
+//Add a class to main
+mainEl.classList.add("flex-ctr");
+
+//Retrieve and store the nav
+const topMenuEl = document.getElementById("top-menu");
+
+//Change the height of the nav
+topMenuEl.style.height = "100%";
+
+//Change the background color of the nav
+topMenuEl.style.background = "var(--top-menu-bg)";
+
+//Add a class to tnav
+topMenuEl.classList.add("flex-around");
+
+//Iterate through the menuLinks Array
+menuLinks.forEach((linkObj) => {
+    //For each link object
+    //console.log(linkObj);
+    //Create an a element
+    const aEl = document.createElement("a");
+    // Set a's href attribute to the linkObj.href
+    aEl.setAttribute("href", linkObj.href);
+    // Set the text to linkObj.text
+    aEl.textContent = linkObj.text;
+    //Append a to the nav
+    topMenuEl.append(aEl);
+  });
